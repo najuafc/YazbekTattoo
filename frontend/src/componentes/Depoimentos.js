a71395c66e222a6afa4a69cc5703daebc6e2f61d
Leila <leilanunesdosanjos@gmail.com>
2023-08-10 17:18:13 -0300

    Depoimentos e Sobre

--- frontend/src/componentes/Depoimentos.js
+++ frontend/src/componentes/Depoimentos.js
@@ -7,9 +7,22 @@ function Sobre() {
     return (
         <div>
             <NavBar/>
+            <div>
+                <h4>Marta Batista</h4>
+                <p>"Minha experiência na Yazbek Tattoo foi incrível! Ele captou exatamente o que eu queria e transformou em uma tatuagem linda e cheia de significado. Recomendo!"</p>
+            </div>
+
+            <div>
+                <h4>Arthur Rodrigues</h4>
+                <p>"Cara, tô só o amor pela minha nova tatuagem da Yazbek Tattoo! O tatuador mandou bem demais e eu tô me achando com esse desenho maneiro na pele."</p>
+            </div>
+            <div>
+                <h4>Lucas Pinto</h4>
+                <p>"Cara, eu fiz minha tatuagem na Yazbek Tattoo e tô impressionado. O tatuador é talentoso demais e ainda me deu um monte de dicas pra cuidar dela. Agora tô viciado em tatuagens!"</p>
+            </div>
             <Footer/>
         </div>
     );
 };
 
-export default Sobre; 
+export default Sobre;
--- frontend/src/componentes/Sobre.js
+++ frontend/src/componentes/Sobre.js
@@ -6,6 +6,18 @@ function Sobre() {
     return (
         <div>
             <NavBar/>
+            <div>
+                <h2>Trajetória</h2>
+                <p>Olá! Sou Ricardo, e minha jornada é uma história de transformação e paixão. Por anos, servi como policial, dedicado a proteger nossa comunidade e manter a lei. Mas, em meio a essa trajetória, encontrei uma paixão inesperada que me levou por um novo caminho.
+
+Minha vida mudou quando conheci Miguel, um tatuador talentoso. Enquanto conversávamos, percebi que a tatuagem era mais do que arte na pele; era uma forma de contar histórias únicas e profundas. Essa conversa despertou algo em mim, e comecei a explorar a arte da tatuagem.
+
+Com o apoio e mentoramento de Miguel, dei um passo corajoso e mergulhei na jornada da tatuagem. Deixei para trás o uniforme de policial e abracei as tintas e agulhas. Cada tatuagem que faço é uma oportunidade de ajudar as pessoas a expressar suas histórias, sentimentos e personalidades.
+
+Hoje, tenho a alegria de administrar meu próprio estúdio de tatuagem, onde posso combinar minha dedicação anterior como policial com minha nova paixão pela arte da tatuagem. Cada tatuagem que crio é uma colaboração, uma manifestação de autoexpressão e uma lembrança duradoura das histórias das pessoas.
+
+Espero poder ajudar você a contar sua história única por meio da arte da tatuagem. Junte-se a mim nesta jornada criativa!</p>
+            </div>
             <Footer/>
         </div>
     );
