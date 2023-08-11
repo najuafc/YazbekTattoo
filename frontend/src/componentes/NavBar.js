37731189189c266047eae0fc90eb6f3b40e9d6a1
Leila <leilanunesdosanjos@gmail.com>
2023-08-11 15:06:30 -0300

    Início estilização da NavBar com MUI

--- frontend/src/componentes/NavBar.js
+++ frontend/src/componentes/NavBar.js
@@ -1,20 +1,34 @@
 import React from "react";
-import Box from '@mui/material/Box';
 import { Link } from "react-router-dom";
+import { styled } from '@mui/system';
+import Grid from '@mui/material/Grid';
 
+const NavList = styled('ul')({
+  display: 'flex',
+  flexDirection: 'row',
+  listStyleType: 'none',
+  padding: 40,
+  width: '100%',
+  justifyContent:'flex-end',
+  backgroundColor: 'lightblue',
+  marginTop: 0
+});
+const NavItem = styled('li')({
+  marginLeft: 16,
+  padding:10
+  
+});
 const NavBar = () => {
   return (
-    <Box display="flex" flexDirection="row">
-      <nav>
-        <ul style={{ listStyleType: "none", display: "flex", flexDirection: "row", padding: 0 }}>
-          <li style={{ marginRight: "10px" }}><Link to="/">Página Inicial</Link></li>
-          <li style={{ marginRight: "10px" }}><Link to="/portfolio">Portfólio</Link></li>
-          <li style={{ marginRight: "10px" }}><Link to="/sobre">Sobre</Link></li>
-          <li style={{ marginRight: "10px" }}><Link to="/agendamento">Agendamento</Link></li>
-          <li><Link to="/Depoimentos">Depoimentos</Link></li>
-        </ul>
-      </nav>
-    </Box>
+      <Grid container justifyContent='start'>
+        <NavList>
+        <NavItem><Link to="/" style={{ textDecoration: 'none', color:'black' }}>Página Inicial</Link></NavItem>
+        <NavItem><Link to="/portfolio" style={{ textDecoration: 'none', color:'black' }}>Portfólio</Link></NavItem>
+        <NavItem><Link to="/sobre" style={{ textDecoration: 'none', color:'black' }}>Sobre</Link></NavItem>
+        <NavItem><Link to="/agendamento" style={{ textDecoration: 'none', color:'black' }}>Agendamento</Link></NavItem>
+        <NavItem><Link to="/depoimentos" style={{ textDecoration: 'none', color:'black' }}>Depoimentos</Link></NavItem>
+        </NavList>
+      </Grid>
   );
 };
 
