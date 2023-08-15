3bee8b411ec968cffa04f1d147b70a815e72a188
Leila <leilanunesdosanjos@gmail.com>
2023-08-15 15:20:45 -0300

    Footer atualizacao cores

--- frontend/src/componentes/Footer.js
+++ frontend/src/componentes/Footer.js
@@ -1,20 +1,35 @@
 import React from 'react';
 import Typography from '@mui/material/Typography';
+import { styled } from '@mui/system';
+
+const FooterContainer = styled('footer')({
+    display: 'flex',
+    backgroundColor: '#a51d1d',
+    padding: '30px',
+    flexDirection: 'column', 
+    alignItems: 'center',
+    marginTop: '5%',
+});
+
+const LinkColor = styled('a')({
+    color: '#b9b9b9',
+    textDecoration: 'underlined',
+})
 
 const Footer = () => {
     return (
-        <footer>
-            <Typography variant="body2" align="center" className="mb-2">
+        <FooterContainer>
+            <Typography variant="body3" align="center" color="#b9b9b9">
             © {new Date().getFullYear()} Yazbek Tattoo. Todos os direitos reservados.
             </Typography>
-            <Typography variant="body2" align="center">
+            <Typography variant="body3" align="center" color="#b9b9b9">
                 Endereço: Rua das Couves, 23 - Florianópolis/SC
             </Typography>
-            <Typography variant="body2" align="center" className="mb-2">
-                Siga-nos no <a href="https://www.instagram.com/yazbektattoo/" style={{ marginRight: '10px'}}>Instagram</a>
-                Contato via <a href="https://www.whatsapp.com/?lang=pt_BR">WhatsApp</a>
+            <Typography variant="body3" align="center" color="#b9b9b9">
+                Siga-nos no <LinkColor href="https://www.instagram.com/yazbektattoo/" style={{ marginRight: '10px'}}>Instagram</LinkColor>
+                Contato via <LinkColor href="https://www.whatsapp.com/?lang=pt_BR">WhatsApp</LinkColor>
             </Typography>
-        </footer>
+        </FooterContainer>
     );
 };
 
