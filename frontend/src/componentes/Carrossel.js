5d252cd26e9cb881c3fc9e9643561200197af6d4
Ana Júlia <anajuliany@gmail.com>
2023-08-15 14:09:45 -0300

    uhul

---
+++ frontend/src/componentes/Carrossel.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import Carousel from 'react-bootstrap/Carousel';
+import FotoCarrossel1 from './imagens/fotocarrossel1.jpg';
+import FotoCarrossel2 from './imagens/fotocarrossel2.jpg';
+import FotoCarrossel3 from './imagens/fotocarrossel3.jpg';
+
+export default function Carrossel() {
+    return (
+      <div style={{ display: 'block', maxWidth: '100%', maxHeight: '200px', margin: '0 auto'}}>
+        <Carousel>
+          <Carousel.Item interval={1500}>
+            <img
+              className="d-block w-100"
+              src={FotoCarrossel1}
+              alt="Imagem Carrossel 1"
+              style={{ maxHeight: '600px', objectFit: 'cover' }}
+            />
+          </Carousel.Item>
+          <Carousel.Item interval={1500}>
+            <img
+              className="d-block w-100"
+              src={FotoCarrossel2}
+              alt="Imagem Carrossel 2"
+              style={{ maxHeight: '600px', objectFit: 'cover' }}
+            />
+          </Carousel.Item>
+          <Carousel.Item interval={1500}>
+            <img
+            className="d-block w-100"
+            src={FotoCarrossel3}
+            alt='Imagem Carrossel 3'
+            style={{ maxHeight: '600px', objectFit: 'cover' }}
+            />
+          </Carousel.Item>
+        </Carousel>
+      </div>
+    );
+  }
--- frontend/src/componentes/PaginaInicial.js
+++ frontend/src/componentes/PaginaInicial.js
@@ -1,14 +1,32 @@
 import * as React from 'react';
 import NavBar from './NavBar';
 import Footer from './Footer'; 
+import Carrossel from './Carrossel';
+import Botao from "./Botao";
 
 function PaginaInicial() {
     return (
         <div>
-            <NavBar/>
-            <Footer/>
+            <div id='header'>
+                <NavBar/>
+                <Carrossel/>
+            </div>
+            <div id='sobre' style={{ marginTop: '400px', marginLeft: '10px'}}>
+                <div>
+                    <h1>Sobre Nós:</h1>
+                    <p>Na Yazbek Tattoo, acreditamos que a tatuagem é uma forma de arte que permite a você contar sua história única através da pele.</p>
+                    <p>Nossos artistas talentosos combinam criatividade e habilidade para criar tatuagens que refletem sua personalidade e paixões.</p> 
+                    <p>Com anos de experiência e um compromisso inabalável com a qualidade, estamos dedicados a transformar seus sonhos em obras de arte duradouras.</p>
+                </div>
+                <div id='botao-sobre'>
+                    <Botao label="Saiba mais" onClick='/sobre'/>
+                </div>
+            </div>
+            <div id='footer'>
+                <Footer/>
+            </div>
         </div>
-    )
-}
+    );
+};
 
 export default PaginaInicial;
