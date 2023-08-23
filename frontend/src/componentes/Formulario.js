825e870c14790b24cb58c85afe779fff6f52bb62
Ana Júlia <anajuliany@gmail.com>
2023-08-23 15:49:07 -0300

    odio

--- frontend/src/componentes/Formulario.js
+++ frontend/src/componentes/Formulario.js
@@ -4,7 +4,6 @@ import Campo from "./Campo";
 import { ThemeProvider } from '@mui/material/styles';
 import theme from './Theme';
 import styled from "@emotion/styled";
-import axios from "axios"; // Importe a biblioteca axios
 
 const Formulario = () => {
     const [nome, setNome] = useState('');
@@ -15,21 +14,14 @@ const Formulario = () => {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-
         const formData = new FormData();
-        formData.append("nome", nome);
-        formData.append("email", email);
-        formData.append("telefone", telefone);
-        formData.append("tamanho", tamanho);
-        formData.append("imagem", referencia);
-
-        try {
-            await axios.post("/agendamento", formData);
-            console.log('deu certo')
-        } catch (error) {
-            console.error(error);
-        }
+        formData.append('nome', nome);
+        formData.append('email', email);
+        formData.append('telefone', telefone);
+        formData.append('tamanho', tamanho);
+        formData.append('referencia', referencia);
     };
+    
 
 const FormularioContainer = styled.form({
         padding: '50px',
@@ -74,16 +66,18 @@ const TextoPaginaAgendamento = styled('h2')({
 return (
     <ContainerCentralizado>
         <div>
-            <TextoPaginaAgendamento>Agende sua tatuagem conosco!<br></br> Preencha o formulário ao lado com suas informações<br></br>
-                    e em breve entraremos em contato para agendar o melhor horário para você.</TextoPaginaAgendamento>
+            <TextoPaginaAgendamento>
+                <h3>Agende sua tatuagem conosco!</h3><br></br> Preencha o formulário ao lado com suas informações<br></br>
+                    e em breve entraremos em contato para agendar o melhor horário para você.
+            </TextoPaginaAgendamento>
         </div>
-            <form action="/agendamento" method="post" enctype="multipart/form-data" onSubmit={handleSubmit}>
+            <form onSubmit={handleSubmit}>
                 <FormularioContainer>
-                    <Campo label="Nome: " value={nome} onChange={(e) => setNome(e.target.value)} style={campoStyle} type='text' name="nome"/>
+                    <Campo label="Nome: " value={nome} onChange={(e) => setNome(e.target.value)} style={campoStyle}/>
                     <Campo label="Email: " value={email} onChange={(e) => setEmail(e.target.value)} style={campoStyle} type='email' name='email'/>
-                    <Campo label="Telefone: " value={telefone} onChange={(e) => setTelefone(e.target.value)} style={campoStyle} type='tel'name='telefone' />
+                    <Campo label="Telefone: " value={telefone} onChange={(e) => setTelefone(e.target.value)} type='tel'name='telefone' />
                     <Campo label="Tamanho da tatuagem (em cm): " value={tamanho} onChange={(e) => setTamanho(e.target.value)} style={campoStyle} type="number" name='tamanho'/>
-                    <Campo label="Imagem de referência: " value={referencia} onChange={(e) => setReferencia(e.target.files[0])} style={campoStyle} type="file" name="imagem"/>
+                    <Campo label="Imagem de referência: " value={referencia} onChange={(e) => setReferencia(e.target.value)} style={campoStyle} type="file" name="imagem"/>
                     <ThemeProvider theme={theme}>
                     <div style={{ display: 'flex', justifyContent: 'center'}}>
                         <Botao label="Enviar" type="submit"/>
