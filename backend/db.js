bbd1d4fb1f71dfc2b14dc7e90e8dcf697b052fbc
Ana Júlia <anajuliany@gmail.com>
2023-08-30 14:26:21 -0300

    bd

--- backend/db.js
+++ backend/db.js
@@ -1,4 +1,4 @@
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize('postgres://postgres:brigadeiro71@@localhost:5432/cliente', {dialect: 'postgres'});
+const sequelize = new Sequelize('postgres://postgres:paodequeijo123@150.162.67.196:5432/agendamentos', {dialect: 'postgres'});
 
 module.exports = sequelize;
